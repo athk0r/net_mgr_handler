e666bb1bc62df6a08e4b1beb1211af103d42201e
Nick Osterhold <nick.osterhold@gmail.com>
2020-11-10 10:44:42 +0100

    add next-hop

--- src/ip4_config.rs
+++ src/ip4_config.rs
@@ -10,7 +10,8 @@ type DbusOptions = HashMap<String, Variant<Box<dyn RefArg>>>;
 pub struct Ip4Config<'a> {
     pub path: dbus::Path<'a>,
     pub ip: String,
-    pub nameserver: String
+    pub nameserver: String,
+    pub next_hop: String,
 }
 
 impl<'a> Ip4Config<'a> {
@@ -20,11 +21,13 @@ impl<'a> Ip4Config<'a> {
         }
         let nameserver = self::Ip4Config::get_nameserver(&p);
         let ip_address = self::Ip4Config::get_ip(&p);
+        let next_hop = self::Ip4Config::get_next_hop(&p);
 
         Some(Ip4Config {
             path: p,
             ip: ip_address.to_string(),
-            nameserver: nameserver.to_string()
+            nameserver: nameserver.to_string(),
+            next_hop: next_hop,
         })
     }
 
@@ -58,6 +61,42 @@ impl<'a> Ip4Config<'a> {
                 let addr_data = data.get_key_value("address").unwrap();
                 let ret = addr_data.1.as_str().clone().unwrap();
                 return ret.to_string();
+            }
+        }
+        "".to_string()
+    }
+
+    /*pub fn get_route_data<'f>(p: &'a dbus::Path) -> Vec<::std::collections::HashMap<String, dbus::arg::Variant<Box<dyn dbus::arg::RefArg + 'static>>>> {
+        use crate::nm_ip4_config::OrgFreedesktopNetworkManagerIP4Config;
+        let con = Connection::new_system().unwrap();
+        let proxy = con.with_proxy("org.freedesktop.NetworkManager", p, Duration::new(5,0));
+        let route_data: Vec<DbusOptions> = proxy.route_data().unwrap();
+        let mut data: Vec<HashMap<String, _>>;
+        for x in &route_data {
+            println!("{:?}", x);
+            let mut hm: HashMap<String, Box<dyn Any>> = HashMap::new();
+            for y in x.keys() {
+                if let Some(s) = x.get(y).unwrap().0.as_str() { hm.insert(y.clone(), Box::new(s)); }
+                else if let Some(i) = x.get(y).unwrap().0.as_i64() { hm.insert(y.clone(), Box::new(i)); }
+                else { hm.insert(y.clone(), Box::<dyn Any>::new(x.get(y).unwrap().0.into())); }
+            }
+            data.push(hm);
+        }
+        println!("");
+        route_data
+    }*/
+
+    fn get_next_hop<'f>(p: &'a dbus::Path) -> String {
+        use crate::nm_ip4_config::OrgFreedesktopNetworkManagerIP4Config;
+        let con = Connection::new_system().unwrap();
+        let proxy = con.with_proxy("org.freedesktop.NetworkManager", p, Duration::new(5,0));
+        let route_data: Vec<DbusOptions> = proxy.route_data().unwrap();
+
+        for data in route_data {
+            if data.contains_key("next-hop") {
+                let next_hop = data.get("next-hop").unwrap();
+                let return_string = next_hop.0.as_str().clone().unwrap();
+                return return_string.to_string();
             }
         }
         "".to_string()
